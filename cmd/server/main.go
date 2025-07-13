bb37720740e2049ea76ba220234bd74883a81f67
julio_filizzola <juliofilizzola@hotmail.com>
2025-07-12 23:56:02 -0300

    feat: Add database migration for GitHubEvent, User, Repository, and PullRequest models

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github-discord-bot/internal/config"
 	"github-discord-bot/internal/db"
+	"github-discord-bot/internal/model"
 )
 
 func main() {
 	cfg := config.Load()
 
 	db.Connect(cfg.DatabaseURL)
-	//db.DB.AutoMigrate()
+	err := db.DB.AutoMigrate(&model.GitHubEvent{}, &model.User{}, &model.Repository{}, &model.PullRequest{})
+	if err != nil {
+		fmt.Printf("erro ao migrar banco de dados: %v\n", err)
+	}
 }
