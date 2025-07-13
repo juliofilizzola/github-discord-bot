b87aeddb6942b7d8fae1a258bf5c8ea0f8fbccc8
julio_filizzola <juliofilizzola@hotmail.com>
2025-07-12 22:33:36 -0300

    refactor: Simplify GORM database connection implementation

--- internal/db/gorm.go
+++ internal/db/gorm.go
@@ -1,26 +1,18 @@
 package db
 
 import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
 )
 
 var DB *gorm.DB
 
-func Connect() {
-	_ = godotenv.Load()
-
-	dsn := os.Getenv("DATABASE_URL")
+func Connect(dsn string) {
 	var err error
+
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatalf("Erro ao conectar no banco: %v", err)
+		log.Fatalf("Failed to connect to database: %v", err)
 	}
-
-	fmt.Println("Banco conectado com sucesso")
 }
