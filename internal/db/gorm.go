69c71e9753360abe2347217d65917f06cadf0c69
julio_filizzola <juliofilizzola@hotmail.com>
2025-07-12 22:12:02 -0300

    feat: Add GORM database connection implementation

---
+++ internal/db/gorm.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	_ = godotenv.Load()
+
+	dsn := os.Getenv("DATABASE_URL")
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Erro ao conectar no banco: %v", err)
+	}
+
+	fmt.Println("Banco conectado com sucesso")
+}
