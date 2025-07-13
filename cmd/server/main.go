9b3f4015d2278fe8480671ca4f450989abff9905
julio_filizzola <juliofilizzola@hotmail.com>
2025-07-13 11:43:14 -0300

    refactor: Simplify server initialization with database and router setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"github-discord-bot/internal/config"
 	"github-discord-bot/internal/db"
-	"github-discord-bot/internal/model"
+	"github-discord-bot/internal/router"
 )
 
 func main() {
-	cfg := config.Load()
-
-	db.Connect(cfg.DatabaseURL)
-	err := db.DB.AutoMigrate(&model.GitHubEvent{}, &model.User{}, &model.Repository{}, &model.PullRequest{})
-	if err != nil {
-		fmt.Printf("erro ao migrar banco de dados: %v\n", err)
+	if err := db.InitializeDatabase(); err != nil {
+		panic(err)
 	}
+	router.Init()
 }
