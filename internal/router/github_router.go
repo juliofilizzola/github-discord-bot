4f8509f493ebb8d43938751ac74c51d36c4b9903
julio_filizzola <juliofilizzola@hotmail.com>
2025-07-13 16:18:41 -0300

    feat: Update GitHub webhook route to use GitHubController for repository details retrieval

--- internal/router/github_router.go
+++ internal/router/github_router.go
@@ -1,9 +1,15 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github-discord-bot/internal/controller"
+	"github-discord-bot/internal/service"
+	"github.com/gin-gonic/gin"
+)
 
 func init() {
+	githubService := service.NewGithubService()
+	discordService := controller.NewGithubController(githubService)
 	RegisterRoutes(func(r *gin.Engine) {
-		r.POST("/github/webhook", func(c *gin.Context) {})
+		r.POST("/github/webhook", discordService.GetRepositoryDetails)
 	})
 }
