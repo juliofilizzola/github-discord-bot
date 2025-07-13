ce57378fbfe0dd4d62ac04726fe98a11cbb8cbcd
julio_filizzola <juliofilizzola@hotmail.com>
2025-07-13 16:18:31 -0300

    feat: Add GitHubService for handling repository details retrieval

--- internal/service/github.go
+++ internal/service/github.go
@@ -1,7 +1,21 @@
 package service
 
-import "io"
+import "github-discord-bot/internal/repository"
 
-func ProcessGitHubPayload(body io.Reader) error {
+type GitHubService struct {
+	repository *repository.GitHubRepository
+}
 
+func NewGithubService() *GitHubService {
+	return &GitHubService{
+		repository: repository.NewGitHubRepository(),
+	}
+}
+
+func (service *GitHubService) GetRepositoryDetails(owner, repo string) (string, error) {
+	existing, err := service.repository.GetRepositoryDetails(owner, repo)
+	if err != nil {
+		return "", err // Outro erro
+	}
+	return existing, nil
 }
