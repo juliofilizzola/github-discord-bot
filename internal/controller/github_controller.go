d4c2f15f87e54172d5531d173f286fe1922bacfc
Julio Filizzola <juliofilizzola@hotmail.com>
2025-07-17 14:12:42 -0300

    feat: Add SaveRepositoryDetails method to GitHubController for processing and saving repository details

--- internal/controller/github_controller.go
+++ internal/controller/github_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/juliofilizzola/github-discord-bot/internal/model"
 	"github.com/juliofilizzola/github-discord-bot/internal/service"
 )
 
@@ -26,4 +27,16 @@ func (c *GitHubController) GetRepositoryDetails(ctx *gin.Context) {
 	}
 
 	ctx.JSON(200, gin.H{"repository": existing})
+}
+
+func (c *GitHubController) SaveRepositoryDetails(ctx *gin.Context) {
+	var body model.GitHubPullRequestEvent
+	if err := ctx.Bind(&body); err != nil {
+		ctx.JSON(400, gin.H{"error": "Invalid request body"})
+		return
+	}
+	if err := c.service.SaveRepositoryDetails(&body); err != nil {
+		ctx.JSON(500, gin.H{"error": "Failed to save repository details"})
+		return
+	}
 }
