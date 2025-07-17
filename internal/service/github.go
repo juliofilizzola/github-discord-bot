93580620a0c252c6ff5253342d7af5b79ebc61a1
Julio Filizzola <juliofilizzola@hotmail.com>
2025-07-17 14:12:52 -0300

    feat: Add SaveRepositoryDetails method to GitHubService for saving event details and publishing to queue

--- internal/service/github.go
+++ internal/service/github.go
@@ -1,6 +1,10 @@
 package service
 
-import "github.com/juliofilizzola/github-discord-bot/internal/repository"
+import (
+	"github.com/juliofilizzola/github-discord-bot/internal/model"
+	"github.com/juliofilizzola/github-discord-bot/internal/queue"
+	"github.com/juliofilizzola/github-discord-bot/internal/repository"
+)
 
 type GitHubService struct {
 	repository *repository.GitHubRepository
@@ -18,4 +22,12 @@ func (service *GitHubService) GetRepositoryDetails(owner, repo string) (string, 
 		return "", err // Outro erro
 	}
 	return existing, nil
+}
+
+func (service *GitHubService) SaveRepositoryDetails(event *model.GitHubPullRequestEvent) error {
+	if err := service.repository.SaveRepositoryDetails(event); err != nil {
+		return err
+	}
+	queue.EventGithub <- event
+	return nil
 }
