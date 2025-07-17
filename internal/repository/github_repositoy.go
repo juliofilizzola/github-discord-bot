ae309e82ed44f29131da3a98818655e83d619cba
Julio Filizzola <juliofilizzola@hotmail.com>
2025-07-17 14:12:35 -0300

    refactor: Replace GitHubEvent references with GitHubPullRequestEvent in GitHubRepository methods

--- internal/repository/github_repositoy.go
+++ internal/repository/github_repositoy.go
@@ -12,14 +12,14 @@ func NewGitHubRepository() *GitHubRepository {
 }
 
 func (r *GitHubRepository) GetRepositoryDetails(owner, repo string) (string, error) {
-	var existing model.GitHubEvent
+	var existing model.GitHubPullRequestEvent
 	if err := db.DB.Where("owner = ? AND repo = ?", owner, repo).First(&existing).Error; err != nil {
 		return "", err // Outro erro
 	}
 	return existing.ID, nil
 }
 
-func (r *GitHubRepository) SaveRepositoryDetails(event *model.GitHubEvent) error {
+func (r *GitHubRepository) SaveRepositoryDetails(event *model.GitHubPullRequestEvent) error {
 	if err := db.DB.Create(event).Error; err != nil {
 		return err
 	}
