5d857090af57abf82c2d9358d2c6943ae806ede4
Julio Filizzola <juliofilizzola@hotmail.com>
2025-08-01 13:17:49 -0300

    feat: Add ConsumeEventGithub function to process GitHub pull request events

--- internal/queue/event_github_queue.go
+++ internal/queue/event_github_queue.go
@@ -3,3 +3,9 @@ package queue
 import "github.com/juliofilizzola/github-discord-bot/internal/model"
 
 var EventGithub = make(chan *model.GitHubPullRequestEvent, 100)
+
+func ConsumeEventGithub() {
+	for event := range EventGithub {
+		println(event)
+	}
+}
