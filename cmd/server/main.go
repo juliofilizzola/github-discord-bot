17597101a59e740b85c67d514673e8cdcf09e77f
Julio Filizzola <juliofilizzola@hotmail.com>
2025-08-01 13:35:39 -0300

    feat: Initialize database and router in main function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/juliofilizzola/github-discord-bot/internal/config"
+import (
+	"github.com/juliofilizzola/github-discord-bot/internal/db"
+	"github.com/juliofilizzola/github-discord-bot/internal/router"
+)
 
 func main() {
-	config.InitializeSystem()
+	if err := db.InitializeDatabase(); err != nil {
+		panic(err)
+	}
+	router.Init()
 }
