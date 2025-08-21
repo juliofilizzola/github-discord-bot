bbc7861d63a7fd45a5cf3443182fe1f5f2147a16
Julio Filizzola <juliofilizzola@hotmail.com>
2025-08-21 14:31:57 -0300

    feat: Add ColorNotification constants for improved notification status handling

---
+++ internal/constants/color.go
@@ -0,0 +1,10 @@
+package constants
+
+type ColorNotification string
+
+const (
+	ColorPending ColorNotification = "#000000"
+	ColorSent    ColorNotification = "#00FF00"
+	ColorFailed  ColorNotification = "#FF0000"
+	ColorSuccess ColorNotification = "#0000FF"
+)
