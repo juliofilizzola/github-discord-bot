27d1ef20507a799e3a1c766213676cad3b360907
Julio Filizzola <juliofilizzola@hotmail.com>
2025-08-21 14:32:37 -0300

    feat: Refactor NotificationStatus to use constants package for improved maintainability

--- internal/model/notification.go
+++ internal/model/notification.go
@@ -1,19 +1,13 @@
 package model
 
-type NotificationStatus string
-
-const (
-	StatusPending NotificationStatus = "pending"
-	StatusSent    NotificationStatus = "sent"
-	StatusFailed  NotificationStatus = "failed"
-)
+import "github.com/juliofilizzola/github-discord-bot/internal/constants"
 
 type Notification struct {
 	BaseModel
-	Message string             `gorm:"type:text;not null"`
-	Status  NotificationStatus `gorm:"type:enum('pending','sent','failed');default:'pending';not null"`
-	Type    string             `gorm:"type:varchar(50);not null"`
-	UserID  uint               `gorm:"not null" json:"user_id" gorm:"column:user_id;foreignKey:UserID;references:ID" ref:"githubUsers"`
+	Message string                       `gorm:"type:text;not null"`
+	Status  constants.NotificationStatus `gorm:"type:enum('pending','sent','failed');default:'pending';not null"`
+	Type    string                       `gorm:"type:varchar(50);not null"`
+	UserID  uint                         `gorm:"not null" json:"user_id" gorm:"column:user_id;foreignKey:UserID;references:ID" ref:"githubUsers"`
 }
 
 func init() {
