b4e298f5d8921441fb27f8333d238a225d770ada
Julio Filizzola <juliofilizzola@hotmail.com>
2025-10-13 08:39:56 -0300

    feat: Add GetUserOwner method to GitHubRepository for fetching GitHub user details by ID

--- internal/repository/github_repositoy.go
+++ internal/repository/github_repositoy.go
@@ -12,6 +12,14 @@ func NewGitHubRepository() *GitHubRepository {
 	return &GitHubRepository{}
 }
 
+func (r *GitHubRepository) GetUserOwner(ownerId string) (*model.GitHubUser, error) {
+	var existing model.GitHubUser
+	if err := db.GetDB().Where("id = ?", ownerId).First(&existing).Error; err != nil {
+		return nil, err
+	}
+	return &existing, nil
+}
+
 func (r *GitHubRepository) GetRepositoryDetails(owner, repo string) (string, error) {
 	var existing model.GitHubEvent
 	if err := db.GetDB().Where("owner = ? AND repo = ?", owner, repo).First(&existing).Error; err != nil {
