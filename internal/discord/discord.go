cd4fd3e58dc9b9e05a350c673f46454b855c0639
julio filizzola <juliofilizzola@hotmail.com>
2025-10-15 07:39:42 -0300

    feat: Add debug logs and refactor SendEmbedToDiscord function
    - Introduced debug logs for better traceability during Discord embed sending.
    - Updated Discord session initialization with enhanced error handling.
    - Refactored SendEmbedToDiscord to use `DiscordConfig` and cleaned obsolete code.

--- internal/discord/discord.go
+++ internal/discord/discord.go
@@ -1,6 +1,8 @@
 package discord
 
 import (
+	"fmt"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/juliofilizzola/github-discord-bot/internal/config"
 	"github.com/juliofilizzola/github-discord-bot/internal/model"
@@ -15,6 +17,8 @@ func DiscordConfig() (*discordgo.Session, error) {
 	cfg := config.Load()
 	discord, err := discordgo.New("Bot " + cfg.DiscordToken)
 	if err != nil {
+
+		println("Error creating Discord session:")
 		return discord, err
 	}
 	return discord, nil
@@ -22,9 +26,16 @@ func DiscordConfig() (*discordgo.Session, error) {
 
 func SendEmbedToDiscord(webhookId string, event *model.GitHubEvent) error {
 	embed := utils.FormatEmbedDiscord(event)
-	cfg := config.Load()
-	token := cfg.DiscordToken
-	_, err := DiscordServer.WebhookExecute(webhookId, token, false, &embed)
+	//cfg := config.Load()
+	//token := cfg.DiscordToken
+	println("Enviando embed para o Discord")
+	fmt.Printf("Webhook ID:%+v\n", embed)
+	println(webhookId)
+	s, err := DiscordConfig()
+	if err != nil {
+		return err
+	}
+	_, err = s.WebhookExecute("1119049844602974339", "xEn_Ok9gsHt0kH0LCMFG-oDM_0NdIixRtEdvPx1Yb1_pUTsgxv4kFPt_rc_OAPMNXcDa", false, &embed)
 	if err != nil {
 		println("Error sending embed to Discord:", err.Error())
 		return err
