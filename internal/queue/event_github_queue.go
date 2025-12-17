f935a60d7ddab98ccd0cac61603264fd8b563751
julio filizzola <juliofilizzola@hotmail.com>
2025-12-17 11:42:11 -0300

    refactor: Rename channel type to WebhookEvent and update Discord webhook logic

--- internal/queue/event_github_queue.go
+++ internal/queue/event_github_queue.go
@@ -5,14 +5,14 @@ import (
 	"github.com/juliofilizzola/github-discord-bot/internal/model"
 )
 
-var EventGithub = make(chan *model.GitHubEvent, 100)
+var EventGithub = make(chan *model.WebhookEvent, 100)
 
 func ConsumeEventGithub() {
 	println("ConsumeEventGithub")
 	for event := range EventGithub {
 		println("Recebido evento do GitHub:")
 		if event != nil {
-			if err := discord.SendEmbedToDiscord("", event); err != nil {
+			if err := discord.SendEmbedToDiscord("xEn_Ok9gsHt0kH0LCMFG-oDM_0NdIixRtEdvPx1Yb1_pUTsgxv4kFPt_rc_OAPMNXcDa", event); err != nil {
 				println("Erro ao enviar embed para Discord:", err.Error())
 			}
 		} else {
